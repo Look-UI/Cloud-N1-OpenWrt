61fb202eac62f5aeec5df2c1555387f6e04bd8f1
会飞的小情书 <71999976+Look-UI@users.noreply.github.com>
2024-08-03 10:13:12 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ cd openwrt
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#更新golang19.x（修复openwrt-21.02/22.03分支的构建）
+git clone https://github.com/sbwml/luci-app-alist package/alist
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
